1f53e5ac3a20d0de52802558a5c022194c396b37
Josie Angelica Carrillo <josieacarrillo@gmail.com>
2019-10-09 18:15:33 -0700

    added states to capture inputted values

--- react-firebase/src/components/TeamMembers.js
+++ react-firebase/src/components/TeamMembers.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
 
 class TeamMembers extends Component {
+    constructor(props)  {
+        super(props);
+
+        this.state = {
+            name: '',
+            age: ''
+        };
+    }
     render() {
         return (
             <div>
                 <label>Friend's Name</label>
-                <input/>
+                <input value={this.state.name} onChange={e => this.setState({name: e.target.value})}/>
                 <br/>
                 <label>Friend's Age</label>
-                <input/>
+                <input value={this.state.age} onChange={e => this.setState({age: e.target.value})}/>
+                <br/>
+                <button>Submit</button>
             </div>
         );
     }
