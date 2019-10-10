6b5d539ae04381c53ee6a085e85e0269e4177251
Josie Angelica Carrillo <josieacarrillo@gmail.com>
2019-10-09 17:36:19 -0700

    added packages needed, creating first component

---
+++ react-firebase/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TeamMembers from './components/TeamMembers';
+
+
+function App() {
+  render(){
+    return (
+
+    );
+  }
+}
+ReactDOM.render(
+    <TeamMembers/>,
+    document.getElementById('app')
+    );
+export default App;
